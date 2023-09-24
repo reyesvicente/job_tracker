1e583d0805f80cfce91e18a17672fb7e6081fba5
Vicente Antonio G. Reyes <icenreyes@yahoo.com>
2023-09-24 18:11:19 +0800

    added jobs app, and update settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "job_tracker.users",
     # Your stuff: custom apps go here
+    "job_tracker.jobs",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
